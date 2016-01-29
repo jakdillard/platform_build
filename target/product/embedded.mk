c3c3f8c13a0fe841ccbcd916864a9fbe7b45093b
jakdillard <jakdillard@gmail.com>
2016-01-28 22:54:27 -0500

    use adaway host 1/2

--- target/product/embedded.mk
+++ target/product/embedded.mk
@@ -91,5 +91,4 @@ PRODUCT_COPY_FILES += \
     system/core/rootdir/init.usb.rc:root/init.usb.rc \
     system/core/rootdir/init.usb.configfs.rc:root/init.usb.configfs.rc \
     system/core/rootdir/init.trace.rc:root/init.trace.rc \
-    system/core/rootdir/ueventd.rc:root/ueventd.rc \
-    system/core/rootdir/etc/hosts:system/etc/hosts
+    system/core/rootdir/ueventd.rc:root/ueventd.rc
